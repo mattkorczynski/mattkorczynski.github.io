4c5dabdd9693715268a654f4c472435ac9e80aac
Matt Korczynski <korczynski.mateusz@gmail.com>
2023-03-20 14:53:06 +0100

    Updated layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './index.css'
 const App = () => {
   return (
     <div className = 'bg'>
-
+      <div className = 'name'>matt korczynski</div>
+      <div className = 'cloud'>
+      </div>
+      <a href = {'http://188.210.223.71:8050/'}>
+        <div className = 'links'>m m - data analysis</div>
+      </a>
+      <a href = {'http://188.210.223.71:8051/'}>
+        <div className = 'links_faro'>betri deildin </div>
+      </a>
     </div>
   )
 }
